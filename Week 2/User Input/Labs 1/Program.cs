ad14b0e056eed148977821ae7f21ac44a12a47b6
George Spurrier <146434469+GeorgeSpurrier@users.noreply.github.com>
2024-11-22 12:55:23 +0000

    Add files via upload

---
+++ Week 2/User Input/Labs 1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * User Input
+ * This program asks the user for their name and age and then outputs a greeting message.
+ */
+
+//Welcome message
+Console.WriteLine("Greeter Program");
+
+// Ask the user for their name store it in a variable
+Console.WriteLine("Hello, What is your name?");
+string name = Console.ReadLine();
+
+// Create a greeting message
+string output = "Hello, " + name;
+
+// Ask the user for their age and store it in a variable
+Console.WriteLine("What is your age?");
+string age = Console.ReadLine();
+
+// Add the age to the greeting message
+output += " you are " + age + " years old!";
+Console.WriteLine(output);
+
